ad0aeafbe3a922845a4e9549d3fa6d7e8fa14692
CalfMoon <kritagyabhattarai1@gmail.com>
2023-04-06 11:07:45 +0545

    wrote unnecessarily complex js that doesn't work properly

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const calculateTimeDifferenceInDays = function(month, day, year) {
-  const inputDate = new Date(`${month}/${day}/${year}`);
-  const difference = Date.now() - inputDate;
-  console.log(inputDate.toDateString());
-  const differenceInDays = parseInt(difference / (24*60*60*1000));
-  return differenceInDays;
+const calcAge = (birthYear, birthMonth, birthDay) => {
+  const birth = dayjs(`${birthYear}-${birthMonth}-${birthDay}`);
+  const today = dayjs();
+  calculateMonthDaysTillDate(birthMonth, birthDay);
+
+  const diffYear = today.diff(birth, "y");
+  const diffMonth = today.diff(birth, "M") - 12 * diffYear;
+  const diffDay = (today.diff(birth, "d") - 365.2425  * diffYear);
+  
+  return [diffYear, diffMonth, diffDay];
 };
 
-const convertHours = function() {
-  const diffrenceInDays = calculateTimeDifferenceInDays(); 
+const calculateDaysInMonth = (month) => dayjs(`${dayjs().year()}-${month}`).daysInMonth();
+
+const calculateMonthDaysTillDate = (birthMonth, birthDate) => {
+  const currentMonth = dayjs().month()+1;
+  const currentDate = dayjs().date();
+
+  let daysBetweenMonths = 0;
+  if(birthMonth<currentMonth){
+    for(let i=birthMonth+1 ; i<currentMonth ; i++){
+      daysBetweenMonths += calculateDaysInMonth(i);
+    };
+  } else if (currentMonth<birthMonth){
+    for(let i = birthMonth+1; i < currentMonth+12; i++){
+      const x = i <= 12 ? 0 : 12;
+      daysBetweenMonths += calculateDaysInMonth(i-x);
+    };
+  };
+  console.log(daysBetweenMonths);
 };
